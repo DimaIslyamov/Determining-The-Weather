9be949b673e53f6f5553c9f024559d324ad17ae2
DImaStary <islyamov.dima@gmail.com>
2021-04-16 20:43:56 +0200

    Updating the app interface and creating a generic request

--- Determining The Weather/Extensions/ViewController+NWMDelegate.swift
+++ Determining The Weather/Extensions/ViewController+NWMDelegate.swift
@@ -7,9 +7,24 @@
 
 import Foundation
 import UIKit
+import CoreLocation
 
 extension ViewController: NetworkWeatherManagerDelegate {
     func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather) {
-        print(currentWeather.cityName)
+        self.updateInterfaceWith(weather: currentWeather)
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let latitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
+        
+        networkWeatherManager.fethcCurrentWeather(forRequestType: .coordinate(latitude: latitude, longitude: longitude))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
     }
 }
--- Determining The Weather/View Controllers/ViewController.swift
+++ Determining The Weather/View Controllers/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
@@ -16,17 +17,38 @@ class ViewController: UIViewController {
     
     var networkWeatherManager = NetworkWeatherManager()
     
+    lazy var locationManager: CLLocationManager = {
+       let lm = CLLocationManager()
+        lm.delegate = self
+        lm.desiredAccuracy = kCLLocationAccuracyKilometer
+        lm.requestWhenInUseAuthorization()
+        return lm
+    }()
+    
     @IBAction func searchPressed(_ sender: UIButton) {
         self.presentSearchAlertController(withTitle: "Enter city name", massage: nil, style: .alert) { city in
-            self.networkWeatherManager.fetchCurrentWeather(forCity: city)
+            self.networkWeatherManager.fethcCurrentWeather(forRequestType: .cityName(city: city))
         }
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         networkWeatherManager.delegate = self
-        networkWeatherManager.fetchCurrentWeather(forCity: "London")
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func updateInterfaceWith(weather: CurrentWeather) {
+        DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = weather.temperatureString
+            self.feelsLikeTemperatureLabel.text = weather.feelsLikeTemperatureString
+            self.weatherIconImageView.image = UIImage(systemName: weather.systemIconNameString)
+        }
     }
 }
 
